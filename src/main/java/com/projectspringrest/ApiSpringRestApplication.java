e0f87ce362755b6caeaa47cc757140ff919db7c2
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2022-11-06 11:07:47 -0300

    Project Spring Initializr

---
+++ src/main/java/com/projectspringrest/ApiSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.projectspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectspringrest/ApiSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectspringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
